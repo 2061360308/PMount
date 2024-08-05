0be8dce1c0d7ee143009e238599a99f9a433df3a
2061360308 <2061360308@qq.com>
2024-08-05 16:39:16 +0800

    feat:管道通信和文件自动下载打开

--- test.py
+++
@@ -1,6 +0,0 @@
-from fuse import FUSE
-
-from internal.fileSystem.cloud_fs import CloudFS
-
-fs = CloudFS("百度网盘")
-FUSE(fs, "E:/hello", foreground=False, nothreads=True, nonempty=False, async_read=False, raw_fi=False)
